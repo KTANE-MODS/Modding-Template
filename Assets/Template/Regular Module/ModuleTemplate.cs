c133e294cfe9259d8e698de05660e4a8a1847ee4
Hawker <kovujacksonbentley@gmail.com>
2025-05-25 16:04:36 -0400

    Add getting edgework

--- Assets/Template/Regular Module/ModuleTemplate.cs
+++ Assets/Template/Regular Module/ModuleTemplate.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using KModkit;
+using System.Linq;
+using HarmonyLib;
 
 public class ModuleTemplate : MonoBehaviour {
 
     static int ModuleIdCounter = 1;
     private int ModuleId;
     private bool ModuleSolved = false;
+    private const string moduleName = "InsertModNameHere"; //change this to the name of your module. This is used for logging
 
     private KMAudio Audio;
     private KMBombInfo Bomb;
     private KMBombModule Module;
 
-
+    //This is all the vanilla edgework, you can delete ones you don't need. Or you can add moded widgets
+    private int battaryCount; //number of batteries on the bomb
+    private int batterHolderCount; //number of battery holders on the bomb
+    private int AABattaryCount { get { return 2 * (battaryCount - batterHolderCount);  } } //number of AA batteries on the bomb
+    private int DBatteryCount { get { return battaryCount - AABattaryCount; } } //number of D batteries on the bomb
+    private string serialNumber; //the serial number of the bomb
+    private string[] litIndicators; //the lit indicators on the bomb
+    private string[] unlitIndicators; //the unlit indicators on the bomb
+    private string[][] portPlates; //the port plates on the bomb (each array has a collection of ports on that plate)
 
     void Awake()
     {
@@ -21,12 +34,36 @@ public class ModuleTemplate : MonoBehaviour {
         Module = GetComponent<KMBombModule>();
 
         //test to check module solving works. When clicked, the module will immedialy solve
+        //This should be deleted when you implement your own solving logic
         GetComponent<KMSelectable>().OnFocus += delegate () { if (!ModuleSolved) Solve(); };
     }
 
     void Start()
     { 
         //Get edgework here
+        battaryCount = Bomb.GetBatteryCount();
+        batterHolderCount = Bomb.GetBatteryHolderCount();
+        serialNumber = Bomb.GetSerialNumber();
+        litIndicators = Bomb.GetOnIndicators().ToArray();
+        unlitIndicators = Bomb.GetOffIndicators().ToArray();
+        portPlates = Bomb.GetPortPlates().ToArray();
+
+        //his is just an example of how to log the edgework. Delete this if your mod does not require it
+        KTANELogging($"Battary Count {battaryCount}");
+        KTANELogging($"Battery Holder Count {batterHolderCount}");
+        KTANELogging($"AA Battery Count {AABattaryCount}");
+        KTANELogging($"D Battery Count {DBatteryCount}");
+        KTANELogging($"Serial Number {serialNumber}");
+        KTANELogging($"Lit Indicators {string.Join(", ", litIndicators)}");
+        KTANELogging($"Unlit Indicators {string.Join(", ", unlitIndicators)}");
+        KTANELogging($"There are {portPlates.Length} port plate(s) on the bomb");
+        for (int i = 0; i < portPlates.Length; i++)
+        {
+            KTANELogging($"Port Plate {i + 1} has {string.Join(", ", portPlates[i])}");
+        }
+       
+        //if edgework is required to solve the module, start calculations here.
+        //Otherwie, you can either do it here, or in Awake
     }
 
     private void Solve()
@@ -60,7 +97,7 @@ public class ModuleTemplate : MonoBehaviour {
     /// <param name="s">What will be logged</param>
     private void KTANELogging(string s)
     {
-        Debug.Log($"[InsertModNameHere #{ModuleId}] {s}");
+        Debug.Log($"[{moduleName} #{ModuleId}] {s}");
     }
 
     #pragma warning disable 414
@@ -69,6 +106,11 @@ public class ModuleTemplate : MonoBehaviour {
 
     IEnumerator ProcessTwitchCommand(string command)
     {
+        //make the command given entirley captialized. To make capitialization redundant
+        //EX: "Solve" and "solve" will be the same command
+        command = command.ToUpperInvariant();
+
+        //set up commands here (I personally perfer regex, but you can use string.Contains or string.StartsWith if you want)
         yield return null;
     }
 
@@ -80,7 +122,7 @@ public class ModuleTemplate : MonoBehaviour {
 
         if (!ModuleSolved)
         {
-            //should be null, if needing the solve counter for something
+            //should be null if needing the solve counter for something
             //otherwise should be true
             yield return true;
         }
